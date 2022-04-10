45cc480e2e0779b2c30719be6ed916c96502eb8e
OwnKng <owen.king@outlook.com>
2022-04-10 17:46:38 +0100

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import { Canvas } from "@react-three/fiber"
+import Board from "./Board"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <div className='App'>
+    <Canvas
+      orthographic
+      camera={{ zoom: 100 }}
+      onCreated={({ camera }) => {
+        camera.position.setFromSphericalCoords(20, Math.PI / 3, Math.PI / 4)
+        camera.lookAt(0, 0, 0)
+      }}
+      shadows
+    >
+      <ambientLight intensity={0.2} />
+      <pointLight position={[0, 20, 0]} intensity={0.6} />
+      <Board />
+    </Canvas>
+  </div>
+)
 
-export default App;
+export default App
